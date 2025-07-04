68e5d6d2896926f98a213d70a5a5eb303c0f2d39
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-07-04 18:27:08 +0200

    Quotation

--- quotations-app/script.js
+++ quotations-app/script.js
@@ -1,17 +1,24 @@
  let formulaire = document.getElementById("formulaire");
 console.log(formulaire)
 
+
 formulaire.addEventListener("submit", function(event) {
         event.preventDefault();
     let text = document.getElementById("citation").value;
     let author = document.getElementById("auteur").value;
     console.log(text,author)
-    
+
+    addQuote(text, author)
 });
 
 
+let compteurReference = 0
+
 
 function addQuote(text,author){
+compteurReference +=1
+document.getElementById("compteur").innerText = compteurReference
+
 
 let paraText = document.createElement('p');
 paraText.classList.add('text')
@@ -21,14 +28,14 @@ let paraAuth = document.createElement('p');
 paraAuth.classList.add('author')
 paraAuth.innerText = author
 
-const divQuote = document.createElement('div');
+const divQuote = document.createElement('div')
 divQuote.classList.add('quote');
 divQuote.appendChild(paraText)
 divQuote.appendChild(paraAuth)
+document.getElementById("quote-list").appendChild(divQuote)
+
 }
 
-let quoteList = document.getElementById("quote-list")
-quoteList.appenchild(divQuote)
 
 
 let number
